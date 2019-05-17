c9cbcbdc5ca81f0e2405aa728298db9d6df2d3fc
Balthasar Hofer <lebalz@outlook.com>
2019-05-18 00:35:35 +0200

    add axios api and endpoint for user to login

---
+++ app/javascript/api/user.ts
@@ -0,0 +1,25 @@
+import api from './base';
+import { AxiosPromise } from 'axios';
+
+export interface User {
+  id: string;
+  email: string;
+  last_login: Date;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface LoginUser extends User {
+  token: string;
+  crypto_key: string;
+}
+
+export function login(email: string, password: string): AxiosPromise<LoginUser> {
+  return api.post(
+    'login',
+    {
+      email: email,
+      password: password
+    }
+  );
+}
