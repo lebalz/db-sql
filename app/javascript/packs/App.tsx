bb888db128744b102d18509bf19ac11efc6ab9bd
Balthasar Hofer <lebalz@outlook.com>
2019-05-18 10:30:08 +0200

    add react router

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -4,21 +4,27 @@ import { Provider, observer } from 'mobx-react';
 import rootStore from '../stores/root_store';
 import DbSql from '../components/DbSql';
 import Login from '../views/Login';
+import { createBrowserHistory } from 'history';
+import { syncHistoryWithStore } from 'mobx-react-router';
+import { Router } from 'react-router';
 
 const AppContent = observer(({ loggedIn }: { loggedIn: boolean }) => (
   <Provider
     rootStore={rootStore}
     sessionStore={rootStore.session}
   >
-    <Fragment>
+    <Router history={history}>
       {
         loggedIn
           ? <DbSql />
           : <Login />
       }
-    </Fragment>
+    </Router>
   </Provider>
 ));
+
+const browserHistory = createBrowserHistory();
+const history = syncHistoryWithStore(browserHistory, rootStore.routing);
 
 @observer
 class App extends React.Component {
--- app/javascript/stores/root_store.ts
+++ app/javascript/stores/root_store.ts
@@ -1,16 +1,21 @@
 import { observable } from 'mobx';
+import { RouterStore } from 'mobx-react-router';
 import SessionStore from './session_store';
 
 export class RootStore {
   stores = observable<any>([]);
 
   session: SessionStore;
+  routing: RouterStore;
 
   @observable initialized = false;
 
   constructor() {
     this.session = new SessionStore(this);
-    this.stores.push(this.session)
+    this.stores.push(this.session);
+
+    this.routing = new RouterStore();
+    this.stores.push(this.routing);
 
     this.initialized = true;
   }
