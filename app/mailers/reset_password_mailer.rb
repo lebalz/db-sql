3b04c924341186db2296b664477fc0960b2c4787
Balthasar Hofer <lebalz@outlook.com>
2019-05-30 14:53:21 +0200

    add reset password mailer

---
+++ app/mailers/reset_password_mailer.rb
@@ -0,0 +1,10 @@
+class ResetPasswordMailer < ApplicationMailer
+  default from: 'no-reply@db-sql.ch'
+  def reset(user)
+    @user = user
+    mail(
+      to: @user.email,
+      subject: "Reset DB SQL Password"
+    )
+  end
+end
