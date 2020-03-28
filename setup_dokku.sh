657a5f62925ac2cd054ce7fb06700904f4a40c3f
Balthasar Hofer <balthasar.hofer@swissdrg.org>
2020-03-28 22:57:38 +0100

    install pgcrypto on dokku server

--- setup_dokku.sh
+++ setup_dokku.sh
@@ -15,33 +15,58 @@ ssh -i ${SSH_KEY} -T ${SSH_USER}@${IP} 'bash -s' < setup_dokku_plugins.sh
 APP_NAME='db-sql'
 DOKKU_REMOTE_NAME='dokku'
 
-
-echo "create app and service"
+echo "# ---------------------------------------------------------------------------- #"
+echo "# ----------------------- create dokku app and services ---------------------- #"
+echo "# ---------------------------------------------------------------------------- #"
 {
 ssh -i ${SSH_KEY} -T ${SSH_USER}@${IP} << EOF
-if [[ -z \$(dokku apps:exists ${APP_NAME}) ]]; then
-  echo "${APP_NAME} exists"
-else
-  dokku apps:create ${APP_NAME}
-  dokku postgres:create ${APP_NAME}
-  dokku postgres:link ${APP_NAME} ${APP_NAME}
-fi
-dokku config:set ${APP_NAME} RAILS_MASTER_KEY=$(cat config/master.key)
-dokku config:set ${APP_NAME} RAILS_SERVE_STATIC_FILES="1"
+  if [[ \$(ls /home/dokku/${APP_NAME}) ]]; then
+    echo "${APP_NAME} exists"
+  else
+    dokku apps:create ${APP_NAME}
+    dokku postgres:create ${APP_NAME}
+    dokku postgres:link ${APP_NAME} ${APP_NAME}
+  fi
+
+  # db connection url - since executed from within the postgres container, change host to localhost
+  DB_URL=\$(dokku config:get db-sql DATABASE_URL | sed 's/@.*:/@localhost:/')
+
+  # ensure postgres extension 'pgcrypto' is installed
+  dokku postgres:enter ${APP_NAME} psql \${DB_URL} -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA public;'
+
+  # configure the rails app
+  dokku config:set ${APP_NAME} RAILS_MASTER_KEY=$(cat config/master.key)
+  dokku config:set ${APP_NAME} RAILS_SERVE_STATIC_FILES="1" RAILS_ENV="production" DOKKU_PROXY_PORT_MAP="http:80:3000" NO_VHOST="0" DOKKU_DOCKERFILE_PORTS="3000"
+
+  # if you need special env's within the buildstep, add those as follows (e.g. the rails master key)
+  # dokku docker-options:add ${APP_NAME} build "--build-arg RAILS_MASTER_KEY=$(cat config/master.key)"
 EOF
 }
 
-echo "# set git remote"
+# check if a dokku remote was already added
 if [[ -z $(git remote show | grep ${DOKKU_REMOTE_NAME}) ]]; then
+  echo "# ---------------------------------------------------------------------------- #"
+  echo "# ------------------------------ add git remote ------------------------------ #"
+  echo "# ---------------------------------------------------------------------------- #"
   git remote add ${DOKKU_REMOTE_NAME} dokku@${IP}:${APP_NAME}
 fi
+
+echo "# ---------------------------------------------------------------------------- #"
+echo "# ----------------------- deploying ${APP_NAME} ------------------------------ #"
+echo "# ---------------------------------------------------------------------------- #"
 git push ${DOKKU_REMOTE_NAME} $(basename $(git symbolic-ref HEAD)):master
 
-
-echo "configure letsencrypt"
+echo "# ---------------------------------------------------------------------------- #"
+echo "# ----------------------- configure letsencrypt ------------------------------ #"
+echo "# ---------------------------------------------------------------------------- #"
 {
 ssh -i ${SSH_KEY} -T ${SSH_USER}@${IP} << EOF
   dokku config:set --no-restart ${APP_NAME} DOKKU_LETSENCRYPT_EMAIL=$(git config user.email)
   dokku letsencrypt ${APP_NAME}
 EOF
 }
+
+
+echo "# ---------------------------------------------------------------------------- #"
+echo "# ------------------------------------ DONE ---------------------------------- #"
+echo "# ---------------------------------------------------------------------------- #"
--- setup_dokku_plugins.sh
+++ setup_dokku_plugins.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
-echo "# Check for dokku postgres plugin"
 
+echo "# ---------------------------------------------------------------------------- #"
+echo "# --------------------------- check dokku plugins ---------------------------- #"
 if [[ -z $(dokku plugin:list | egrep -oh "postgres.+enabled")  ]]; then
   if [[ -z $(dokku plugin:list | egrep -oh "postgres.+disabled") ]]; then
-    echo "Install dokku postgres plugin"
+    echo "# ----------------------- Install dokku postgres plugin ---------------------- #"
     dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres
   else
-    echo "Enable dokku postgres plugin"
+    echo "# ----------------------- Enable dokku postgres plugin ----------------------- #"
     dokku plugin:enable postgres
   fi
 else
-  echo "postgres already installed"
+  echo "# ----------------------- postgres already installed ------------------------- #"
 fi
 
 echo "# Check for dokku letsencrypt plugin"
 if [[ -z $(dokku plugin:list | egrep -oh "letsencrypt.+enabled")  ]]; then
   if [[ -z $(dokku plugin:list | egrep -oh "letsencrypt.+disabled") ]]; then
-    echo "Install dokku letsencrypt plugin"
+    echo "# ----------------------- install dokku letsencrypt -------------------------- #"
     dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
   else
-    echo "Enable dokku letsencrypt plugin"
+    echo "# -------------------- Enable dokku letsencrypt plugin ----------------------- #"
     dokku plugin:enable letsencrypt
   fi
 else
-  echo "letsencrypt already installed"
+  echo "# -------------------- letsencrypt already installed ------------------------- #"
 fi
+echo "# ---------------------------------------------------------------------------- #"
