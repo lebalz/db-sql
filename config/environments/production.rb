7bdbaad90563b2685464f55b5d883f7ad9f28c35
Balthasar Hofer <balthasar.hofer@swissdrg.org>
2020-03-28 15:59:15 +0100

    remove yarn integrity check on productive startup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,4 +125,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "https://db-sql.ch" }
 
+  config.webpacker.check_yarn_integrity = false
 end
