4e83399aeeee98612982eeebccb86c8172948508
Balthasar Hofer <balthasar.hofer@swissdrg.org>
2020-05-19 00:03:14 +0200

    only display pg db's with connect privilege for the current user

--- lib/queries/psql/databases.sql
+++ lib/queries/psql/databases.sql
@@ -1,1 +1,6 @@
-SELECT DISTINCT datname AS "databases" FROM pg_database WHERE datistemplate = false;
+SELECT datname AS "databases"
+FROM pg_database
+WHERE
+  datistemplate = false
+  AND
+  has_database_privilege(datname, 'CONNECT');
