dfb1de982c73d93a4d7b3743d115c1fd6bc6b624
LeBalz <lebalz@outlook.com>
2023-08-20 13:44:52 +0200

    use caching for og preview

--- app/api/resources/og_meta.rb
+++ app/api/resources/og_meta.rb
@@ -8,20 +8,25 @@ module Resources
       desc 'Get og:image from url'
       post do
         url = params[:url]
-        response = HTTP.get(url)
-        if response.status != 200
-          return error!('Url not found', 404)
-        end
-        doc = Nokogiri::HTML(response.body.to_s)
-        meta = {}
-        ['og:image', 'og:site_name', 'og:title', 'og:description'].each do |m|
-          prop = doc.css("meta[property='#{m}']")
-          if !prop.empty?
-            meta[m[3..].to_sym] = prop.first.attributes["content"].content
+        _meta = Rails.cache.fetch("#{url}", expires_in: 2.hours) do
+          p "Fetching #{url}"
+          response = HTTP.get(url)
+          if response.status != 200
+            return error!('Url not found', 404)
           end
+          doc = Nokogiri::HTML(response.body.to_s)
+          meta = {}
+          ['og:image', 'og:site_name', 'og:title', 'og:description'].each do |m|
+            prop = doc.css("meta[property='#{m}']")
+            if !prop.empty?
+              meta[m[3..].to_sym] = prop.first.attributes["content"].content
+            end
+          end
+          meta
         end
+
         present(
-          meta,
+          _meta,
           with: Entities::OGMeta
         )
       end
